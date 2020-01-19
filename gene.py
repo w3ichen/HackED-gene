6b51488d6b2e3dd688e4d9b7a55b0bc5b75b8d0a
weichen <weichen@github.com>
2020-01-18 23:00:09 -0700

    foe eddie

--- gene.py
+++ gene.py
@@ -5,13 +5,21 @@ import numpy as np
 clinical_data = requests.get("https://www.cbioportal.org/webservice.do?cmd=getClinicalData&case_set_id=gbm_tcga_pan_can_atlas_2018_all")
 genomic_data = requests.get("https://www.cbioportal.org/webservice.do?cmd=getProfileData&case_set_id=gbm_tcga_pan_can_atlas_2018_rna_seq_v2_mrna&genetic_profile_id=gbm_tcga_pan_can_atlas_2018_rna_seq_v2_mrna_median_Zscores&gene_list=CDK1")
 
+genomic = list(genomic_data.text)
 
-# with open("clinical_data.txt","w") as file:	
-# 	file.write(clinical_data.text)
+# delete the two lines of comments on the top
+del[genomic[0:134]]
+
+genomic = "".join(genomic)
+
+
+with open("clinical_data.txt","w") as file:	
+	file.write(clinical_data.text)
 with open("genomic_data.txt","w") as file:	
-	file.write(genomic_data.text)
+	file.write(genomic)
 
 
-# print(pd.read_csv("clinical_data.txt", delimiter = '\t'))
-print(pd.read_csv("genomic_data.txt", delimiter = '\t'))
+print(pd.read_csv("clinical_data.txt", delimiter = '\t'))
+print(np.transpose(pd.read_csv("genomic_data.txt", delimiter = '\t')))
+
 
--- merge.py
+++ merge.py
@@ -2,4 +2,5 @@ def merge(main, secondary):
 	main_list = list(main.keys())
 	for key in main_list:
 		main[key].extend(secondary[key])
+
 		
