4a8f85cdef5afed70adccdec6faaa6a6af67dc7b
weichen <weichen@github.com>
2020-01-18 17:19:47 -0700

    testing

--- gene.py
+++ gene.py
@@ -1,8 +1,17 @@
 import requests
+from bs4 import BeautifulSoup as soup
+import json
+import bs4
 
-clinical_data = requests.get("https://www.cbioportal.org/webservice.do?cmd=getClinicalData&case_set_id=gbm_tcga_pan_can_atlas_2018_all")
-genomic_data = requests.get("https://www.cbioportal.org/webservice.do?cmd=getProfileData&case_set_id=gbm_tcga_pan_can_atlas_2018_rna_seq_"
-                            "v2_mrna&genetic_profile_id=gbm_tcga_pan_can_atlas_2018_rna_seq_v2_mrna_median_Zscores&gene_list=CDK1")
+# headers={'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'}
 
-print(clinical_data.text)
-print(genomic_data.text)
+
+# clinical_data = requests.get("https://www.cbioportal.org/webservice.do?cmd=getClinicalData&case_set_id=gbm_tcga_pan_can_atlas_2018_all",headers=headers)
+# genomic_data = requests.get("https://www.cbioportal.org/webservice.do?cmd=getProfileData&case_set_id=gbm_tcga_pan_can_atlas_2018_rna_seq_"
+#                             "v2_mrna&genetic_profile_id=gbm_tcga_pan_can_atlas_2018_rna_seq_v2_mrna_median_Zscores&gene_list=CDK1")
+
+
+
+# page_Soup=soup(clinical_data.text,'xml')
+# print(page_Soup,json())
+
